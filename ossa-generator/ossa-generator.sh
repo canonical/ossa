d851c514a547b24ed053acd681cb58aef09c9b59
ThinGuy <19372989+ThinGuy@users.noreply.github.com>
2020-04-23 09:01:15 -0700

    make generator show release

--- ossa-generator/ossa-generator.sh
+++ ossa-generator/ossa-generator.sh
@@ -45,7 +45,50 @@ echo export OSSA_HOST=$(grep -oP '(?<=madison\.)[^$]+' <<< "${OSSA_ARCHIVE//.tgz
 [[ -s ${DPKG_L} ]] && { awk '/^ii/{print $2"\t"$3}' ${DPKG_L}|tee 1>/dev/null ${OSSA_WORKDIR}/manifest.${OSSA_HOST}; }
 echo "export OSSA_MANIFEST=${OSSA_WORKDIR}/manifest.${OSSA_HOST}"|tee 1>/dev/null -a ${OSSARC}
 [[ -s ${OSSA_RC} ]] && source ${OSSA_RC}
+#!/bin/bash
+show-release-info() {
+(printf "\nRelease|Code Name|Release Type|Standard Support Ends In...|ESM Ends In...\n"
+(UBU_URLS=( http://releases.ubuntu.com http://old-releases.ubuntu.com/releases )
+printf '%s\n' ${UBU_URLS[@]}|\
+(xargs -rn1 -P0 -I{} curl -sSlL {}|\
+awk -vRS=">|<" '/^Ubuntu.[0-9]+.*'${OSSA_CODENAME^}'/{gsub(/\(|\).*$|LTS |Beta /,"");gsub(/\.[0-9]$/,"",$2);split($2,a,/\./);print $0}'|sort -uV) |\
+while read -er DIST RELEASE CN1 CN2;do
+[[ $((${RELEASE%%\.*} % 2)) -eq 1 || ! ${RELEASE##*\.} = 04 ]] && { export FLAG=$(printf "\e[38;2;160;160;160;3mInterim\e[0m") SL=182 SD=days EL=0; }
+[[ $((${RELEASE%%\.*} % 2)) -eq 0 && ${RELEASE%%\.*} -le 10 && ${RELEASE%%\.*} -ge 6 ]] && [[ ${RELEASE##*\.} = 06 || ${RELEASE##*\.} = 04 ]] && { export FLAG=$(printf "LTS \e[38;2;160;160;160m(3 yrs)\e[0m") SL=3 SD=years EL=0; }
+[[ $((${RELEASE%%\.*} % 2)) -eq 0 && ${RELEASE%%\.*} -le 16 && ${RELEASE%%\.*} -ge 12 && ${RELEASE##*\.} = 04 ]] && { export FLAG=$(printf "LTS \e[38;2;160;160;160m(5 yrs)\e[0m") SL=5 SD=years EL=8; }
+[[ $((${RELEASE%%\.*} % 2)) -eq 0 && ${RELEASE%%\.*} -ge 18 && ${RELEASE##*\.} = 04 ]] && { export FLAG=$(printf "LTS \e[38;2;160;160;160m(10 yrs)\e[0m") SL=5 SD=years EL=10; }
+EOS_END=$(date -d "$(date -d "$([[ ${RELEASE%%\.*} -le 99 ]] && echo -n $((${RELEASE%%\.*}+2000)) || echo -n $((${RELEASE%%\.*}+3000)))$(printf '%02d' $((10#${RELEASE##*\.}+1)))01 -1 day" +%Y%m%d) +${SL} ${SD}" +%Y%m%d)
+EOL_END=$(date -d "$(date -d "$([[ ${RELEASE%%\.*} -le 99 ]] && echo -n $((${RELEASE%%\.*}+2000)) || echo -n $((${RELEASE%%\.*}+3000)))$(printf '%02d' $((10#${RELEASE##*\.}+1)))01 -1 day" +%Y%m%d) +${EL} ${SD}" +%Y%m%d)
+EOS_DAYS_REMAINING=$((($(date -d ${EOS_END} +%s)-$(date +%s))/(60*60*24)))
+[[ ${EL} = 0 ]] && EOL_DAYS_REMAINING=0 || EOL_DAYS_REMAINING=$((($(date -d ${EOL_END} +%s)-$(date +%s))/(60*60*24)))
+
+if [[ ${EOS_DAYS_REMAINING} -le 0 ]];then
+EOS_DAYS_REMAINING=$(printf "\e[38;2;160;160;160;3mExpired\e[0m")
+elif [[ ${EOS_DAYS_REMAINING} -le 30 && ${EOS_DAYS_REMAINING} -gt 0 ]];then
+EOS_DAYS_REMAINING=$(printf "\e[38;2;255;0;0m${EOS_DAYS_REMAINING} days ($(date -d "$(date +${EOS_END})" '+%d/%B/%Y'))\e[0m")
+elif [[ ${EOS_DAYS_REMAINING} -lt 365 && ${EOS_DAYS_REMAINING} -gt 0 ]];then
+EOS_DAYS_REMAINING=$(printf "\e[38;2;255;225;0m${EOS_DAYS_REMAINING} days ($(date -d "$(date +${EOS_END})" '+%d/%b/%Y'))\e[0m")
+elif [[ ${EOS_DAYS_REMAINING} -gt 365 && ${EOS_DAYS_REMAINING} -gt 0 ]];then
+EOS_DAYS_REMAINING=$(printf "\e[38;2;0;235;0m${EOS_DAYS_REMAINING} days ($(date -d "$(date +${EOS_END})" '+%d/%b/%Y'))\e[0m")
+fi
+
+if [[ ${EOL_DAYS_REMAINING} -le 0 ]];then
+EOL_DAYS_REMAINING=$(printf "\e[38;2;160;160;160;3mExpired\e[0m")
+elif [[ ${EOL_DAYS_REMAINING} -le 30 ]];then
+EOL_DAYS_REMAINING=$(printf "\e[38;2;225;0;0m${EOL_DAYS_REMAINING} days ($(date -d "$(date +${EOL_END})" '+%d/%b/%Y'))\e[0m")
+elif [[ ${EOL_DAYS_REMAINING} -lt 365 ]];then
+EOL_DAYS_REMAINING=$(printf "\e[38;2;255;200;0m${EOL_DAYS_REMAINING} days ($(date -d "$(date +${EOL_END})" '+%d/%b/%Y'))\e[0m")
+elif [[ ${EOL_DAYS_REMAINING} -gt 365 ]];then
+EOL_DAYS_REMAINING=$(printf "\e[38;2;0;235;0m${EOL_DAYS_REMAINING} days ($(date -d "$(date +${EOL_END})" '+%d/%b/%Y'))\e[0m")
+fi
+[[ ${EL} = 0 ]] && EOL_DAYS_REMAINING=$(printf "\e[38;2;160;160;160;3mN/A\e[0m")
+echo "$DIST $RELEASE|$CN1 $CN2|$FLAG|${EOS_DAYS_REMAINING}|${EOL_DAYS_REMAINING}"
+done))|column -nexts"|"|sed '2s/^.*$/'$(printf "\e[1m&\e[0m")'/g'
+echo
+};export show-release-info
+
 clear
+show-release-info
 # Show package breakdown by component and suite
 if [[ -f ${APT_MADISON} ]];then
 	declare -ag COMPONENTS=(main universe multiverse restricted)
@@ -87,15 +130,13 @@ fi
 ######################
 # DOWNLOAD OVAL DATA #
 ######################
-
+printf "\e[2GChecking the availbility of OVAL Data for Ubuntu ${OSSA_CODENAME}\n"
+printf "\e[4GOVAL URL: https://people.canonical.com/~ubuntu-security/oval/oci.com.ubuntu.${OSSA_CODENAME}.cve.oval.xml.bz2\n"
 export OVAL_URI="https://people.canonical.com/~ubuntu-security/oval/oci.com.ubuntu.${OSSA_CODENAME}.cve.oval.xml.bz2"
 export TEST_OVAL=$(curl -slSL --connect-timeout 5 --max-time 20 --retry 5 --retry-delay 1 -w %{http_code} -o /dev/null ${OVAL_URI} 2>&1)
 [[ ${OSSA_DEBUG} = true ]] && { echo "${TEST_OVAL}"; }
 if [[ ${TEST_OVAL:(-3)} -eq 404 ]];then
 	printf "\e[2GOVAL data file for Ubuntu ${OSSA_CODENAME^} is not available. Skipping download.\n"
-elif [[ ${TEST_OVAL:(-3)} -eq 200 ]];then
-	printf "\e[2GDownloading OVAL data for Ubuntu ${OSSA_CODENAME^}\n"
-	curl -slSL --connect-timeout 10 --max-time 30 --retry 3 --retry-delay 2 ${OVAL_URI} -o- |bunzip2 -d|tee 1>/dev/null ${OSSA_WORKDIR}/$(basename ${OVAL_URI//.bz2})
 elif [[ ${TEST_OVAL:(-3)} -eq 000 ]];then
 	printf "\e[2GNetwork issues prevented download of OVAL data for Ubuntu ${OSSA_CODENAME}\n"
 fi
@@ -104,7 +145,11 @@ fi
 # PERFORM CVE SCAN #
 ####################
 
+
 if [[ ${TEST_OVAL:(-3)} -eq 200 ]];then
-	printf "\e[2GInitiating CVE Scan against package manifest collected from host ${OSSA_HOST} running Ubuntu ${OSSA_CODENAME^}...\n\n"
-	oscap oval eval --report ${OSSA_WORKDIR}/oscap-cve-scan-report.${OSSA_HOST,,}.html ${OSSA_WORKDIR}/$(basename ${OVAL_URI//.bz2})|awk -vF=0 -vT=0 '{if ($NF=="false") F++} {if ($NF=="true") T++} END {print "CVE Scan Results (Summary)\nCommon Vulnerabilities Addressed: "F"\nCurrent Vulnerability Exposure: "T}'|tee ${OSSA_WORKDIR}/cve-stats.${OSSA_HOST,,}|sed 's/^.*$/ &/g'
+	printf "\e[4GInitiating CVE Scan:\n\e[6GOVAL Data OS: Ubuntu ${OSSA_CODENAME^}\n"
+	curl -slSL --connect-timeout 10 --max-time 30 --retry 3 --retry-delay 2 ${OVAL_URI} -o- |bunzip2 -d|tee 1>/dev/null ${OSSA_WORKDIR}/$(basename ${OVAL_URI//.bz2})
+printf "\e[6GOVAL Scan Type:  Package manifest\n\e[6GOrigin Hostname: ${OSSA_HOST}\n\e[6GOrigin Codename: ${OSSA_CODENAME^}\n\n"
+	oscap oval eval --report ${OSSA_WORKDIR}/oscap-cve-scan-report.${OSSA_HOST,,}.html ${OSSA_WORKDIR}/$(basename ${OVAL_URI//.bz2})|awk -vF=0 -vT=0 '{if ($NF=="false") F++} {if ($NF=="true") T++} END {print "CVE Scan Results (Summary)\n  Common Vulnerabilities Addressed: "F"\n  Current Vulnerability Exposure: "T}'|tee ${OSSA_WORKDIR}/cve-stats.${OSSA_HOST,,}|sed 's/^.*$/ &/g'
 fi
+echo;echo
