0376380a6a7b0a9126a1946ca7374ad475dba854
ThinGuy <19372989+ThinGuy@users.noreply.github.com>
2020-04-27 10:38:36 -0700

    Add /etc/hosts and /etc/hostname to collector

--- ossa-collector/collector.sh
+++ ossa-collector/collector.sh
@@ -9,6 +9,7 @@ echo -e "\nRunning OSSA ${TITLE} against ${SSH_HOST##*@}. Please wait..."
 # Start Timer
 TZ=UTC export NOW=$(date +%s)sec
 ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o CheckHostIP=no ${SSH_HOST} bash -c '\
+declare -ag FILES=();
 trap '"'"'(cd /tmp && rm -f "${FILES[@]//\/tmp\/}");trap - INT TERM EXIT QUIT KILL;exit 0'"'"' INT TERM EXIT QUIT KILL;
 declare -ag FILES=();
 export SFX=$(hostname -s)
@@ -16,7 +17,7 @@ export SOURCES_LIST=$(apt-config dump|awk '"'"'/^Dir[ ]|^Dir::Etc[ ]|^Dir::Etc::
 export SOURCES_LIST_D=$(apt-config dump|awk '"'"'/^Dir[ ]|^Dir::Etc[ ]|^Dir::Etc::sourcep/{gsub(/"|;$/,"");print "/"$2}'"'"'|sed -r '"'"':a;N;$! ba;s/\/\/|\n//g'"'"')
 export DPKG_STATUS=$(apt-config dump|awk '"'"'/^Dir::State::status[ ]/{gsub(/"|;$/,"");print $2}'"'"')
 export APT_LISTS_DIR=$(apt-config dump|awk '"'"'/^Dir[ ]|^Dir::State[ ]|^Dir::State::lists[ ]/{gsub(/"|;$/,"");print "/"$2}'"'"'|sed -r '"'"':a;N;$! ba;s/\/\/|\n//g'"'"')
-find 2>/dev/null ${SOURCES_LIST%/*} ${SOURCES_LIST_D} ${DPKG_STATUS%/*} ${APT_LISTS_DIR%*/} -type f -regextype "posix-extended" -iregex '"'"'.*(status$|\.list$|Release$|Packages$)'"'"'|sort -uV|sed -r "/mirror.list|$(dirname ${DPKG_STATUS//\//\\\/})/info/d"|tar 2>/dev/null -cf /tmp/apt-files.${SFX}.tar --files-from -
+find 2>/dev/null ${SOURCES_LIST%/*} ${SOURCES_LIST_D} ${DPKG_STATUS%/*} ${APT_LISTS_DIR%*/} /etc/hosts /etc/hostname -type f -regextype "posix-extended" -iregex '"'"'.*(hosts$|hostname$|status$|\.list$|Release$|Packages$)'"'"'|sort -uV|sed -r "/mirror.list|$(dirname ${DPKG_STATUS//\//\\\/})/info/d"|tar 2>/dev/null -cf /tmp/apt-files.${SFX}.tar --files-from -
 [[ $? -eq 0 && -f /tmp/apt-files.${SFX}.tar ]] && FILES+=( "apt-files.${SFX}.tar" )
 if (command -v netstat &>/dev/null);then NETSTAT=$(command -v netstat);elif (command -v ss &>/dev/null);then NETSTAT=$(command -v ss);else NETSTAT="";fi
 [[ -n ${NETSTAT} ]] && { $NETSTAT 2>/dev/null -an > /tmp/netstat-an.${SFX};[[ $? -eq 0 && -f /tmp/netstat-an.${SFX} ]] && FILES+=( "netstat-an.${SFX}" ); }
