fa31e7549b7a0795f2924bf66283b3e9d0fe6799
ThinGuy <19372989+ThinGuy@users.noreply.github.com>
2020-04-26 21:44:29 -0700

    Adding functions file to keep scripts smaller

--- ossa-generator/ossa-generator.sh
+++ ossa-generator/ossa-generator.sh
@@ -1,107 +1,71 @@
 #!/bin/bash
 
 export PROG_DIR="$( cd "$( dirname "${0}" )/" && pwd )"
+[[ -z ${1} || ${1} =~ -h || ! -f ${1} ]] && { echo -en "Usage: ./${0##*/} <path to ossa-lite archive>\nNote: sudo access is required\n" 1>&2;exit 2; }
 
-test-oval-url() {
-	export URI="https://people.canonical.com/~ubuntu-security/oval/oci.com.ubuntu.bionic.cve.oval.xml.bz2"
-	export C=1 M=10 T=000
-	echo "Testing connectivity to https://people.canonical.com/~ubuntu-security/oval/oci.com.ubuntu.bionic.cve.oval.xml.bz2"
-	until [[ ${T:(-3)} -eq 200 ]];do 
-		T="$(curl -slSL --connect-timeout ${C} --max-time ${M} -w %{http_code} --retry 0 -o /dev/null ${URI} 2>&1)"
-		C=$((C+1)) M=$((M+1))
-		[[ ${TL:(-3)} -eq 200 ]] && { echo "\rHTTP Code: $T Time Values: connect-timeout=$C max-time=$M\e[K\n\n"; } || { echo -en "\rHTTP Code: $T Time Values: connect-timeout=$C max-time=$M\e[K"; }
-		sleep 1
-	done
-	echo
-	export C=${C} M=${M}
-	return ${T:(-3)}
-};export -f test-oval-url
-[[ -n ${1} && ${1} =~ -t ]] && { test-oval-url 1>&2;exit 0; }
-[[ -z ${1} || ${1} =~ -h || ! -f ${1} ]] && { echo -en "Usage: ./${0##*/} -t|--test Tests access to OVAL Data URL\n\n\e[7G./${0##*/} <path to ossa-lite archive>\nNote: sudo access is required\n" 1>&2;exit 2; }
-[[ -f ${1} ]] && export OSSA_ARCHIVE=${1}
+[[ -f ${PROG_DIR}/../lib/ossa_functions ]] && source ${PROG_DIR}/../lib/ossa_functions 
 
 #Root/sudo check
 [[ ${EUID} -eq 0 ]] && { export SCMD="";[[ ${DEBUG} = True ]] && { printf "\e[38;2;255;200;0mDEBUG: User is root\n\n"; };exit; } || { [[ ${EUID} -ne 0 && -n $(id|grep -io sudo) ]] && export SCMD=sudo || { export SCMD="";printf "\e[38;2;255;0;0mERROR: User (${USER}) does not have sudo permissions. Quitting.\n\n";exit 5; }; }
 
-printf "Using OSSA data from ${OSSA_ARCHIVE}\n"
+
+[[ -f ${1} ]] && export OSSA_ARCHIVE=${1}
+[[ ${VERBOSE} = true ]] && { printf "Using OSSA data from ${OSSA_ARCHIVE}\n"; }
+
+
 # set ossa user
-printf "Setting \${OSSA_USER} to "
-[[ -z ${OSSA_USER} ]] && export OSSA_USER=${OSSA_USER:-$(id -un 1000)};echo $OSSA_USER
-# set ossa group
-printf "Setting \${OSSA_GROUP} to "
+[[ ${VERBOSE} = true ]] && { printf "Setting \${OSSA_USER} to "; }
+[[ -z ${OSSA_USER} ]] && export OSSA_USER=${OSSA_USER:-$(id -un 1000)};[[ ${VERBOSE} = true ]] && { echo $OSSA_USER; }
+
+# set ossa group 
+[[ ${VERBOSE} = true ]] && { printf "Setting \${OSSA_GROUP} to "; }
 [[ -z ${OSSA_GROUP} ]] && export ${OSSA_GROUP:-$(id -gn 1000)};echo ${OSSA_GROUP}
+
 # main ossa directory
-export OSSA_DIR=/opt/ossa;printf "Setting \${OSSA_DIR} to ${OSSA_DIR}\n"
+export OSSA_DIR=/opt/ossa;[[ ${VERBOSE} = true ]] && { printf "Setting \${OSSA_DIR} to ${OSSA_DIR}\n"; }
+
 # working directory for this assesment 
-export OSSA_WORKDIR="${OSSA_DIR}/$(basename ${OSSA_ARCHIVE%.*})";printf "Setting \${OSSA_WORKDIR} to ${OSSA_WORKDIR}\n"
+export OSSA_WORKDIR="${OSSA_DIR}/$(basename ${OSSA_ARCHIVE%.*})";[[ ${VERBOSE} = true ]] && { printf "Setting \${OSSA_WORKDIR} to ${OSSA_WORKDIR}\n"
+
 # set rc file
-export OSSA_RC=${OSSA_WORKDIR}/ossarc;printf "Setting \${OSSA_RC} to ${OSSA_RC}\n"
+export OSSA_RC=${OSSA_WORKDIR}/ossarc;[[ ${VERBOSE} = true ]] && { printf "Setting \${OSSA_RC} to ${OSSA_RC}\n"; }
+
 # Make working directroy and extract the archive
-printf "Exractive OSSA data from ${OSSA_ARCHIVE}\n"
+[[ ${VERBOSE} = true ]] && { printf "Exractive OSSA data from ${OSSA_ARCHIVE}\n"; }
 ${SCMD} mkdir -p ${OSSA_WORKDIR} && ${SCMD} tar -C ${OSSA_WORKDIR} -xzf ${OSSA_ARCHIVE}
+
 # set workdir permisssions
 if [[ -n ${OSSA_WORKDIR} && -d ${OSSA_WORKDIR} ]];then
-	printf "Setting permissions on directory ${OSSA_WORKDIR}\n"
+	[[ ${VERBOSE} = true ]] && { printf "Setting permissions on directory ${OSSA_WORKDIR}\n"; }
 	${SCMD} find ${OSSA_WORKDIR} \( -type d -exec chmod u+rwx,g+rwx,o+rx {} \; -o -type f -exec chmod u+rw,g+rw,o+r {} \; \)
-	printf "Setting ownership on directory ${OSSA_WORKDIR} to ${OSSA_USER}.${OSSA_GROUP}\n"
+	[[ ${VERBOSE} = true ]] && { printf "Setting ownership on directory ${OSSA_WORKDIR} to ${OSSA_USER}.${OSSA_GROUP}\n"; }
 	${SCMD} find ${OSSA_WORKDIR} \( -type d -exec chown ${OSSA_USER}.${OSSA_GROUP} {} \; -o -type f -exec chown ${OSSA_USER}:${OSSA_GROUP} {} \; \)
 fi
+
 # populate rc file with current OSSA* env settings
-printf "Adding current OSSA env variables to ${OSSA_RC}\n"
+[[ ${VERBOSE} = true ]] && { printf "Adding current OSSA env variables to ${OSSA_RC}\n"; }
 set|grep ^OSSA|sed 's|^|export |g'|tee 1>/dev/null ${OSSA_RC}
+
 # add file locations to rc
-printf "Adding OSSA file variables to ${OSSA_RC}\n"
+[[ ${VERBOSE} = true ]] && { printf "Adding OSSA file variables to ${OSSA_RC}\n"; }
 (cd ${OSSA_WORKDIR} && ls -1|xargs -rn1 -P0 bash -c 'V=${0%%.*};V=${V^^};V=${V//-/_};echo $V="$( cd "$( dirname "${0}" )/" && pwd )/${0##*/}"'|tee 1>/dev/null -a ${OSSA_RC})
-[[ -s ${OSSA_RC} ]] && { printf "Sourcing RC file (${OSSA_RC})\n";source ${OSSA_RC}; }
+[[ -s ${OSSA_RC} ]] && { [[ ${VERBOSE} = true ]] && printf "Sourcing RC file (${OSSA_RC})\n";source ${OSSA_RC}; }
 [[ -f ${LSB_RELEASE} ]] && sed 's/DISTRIB/OSSA/g' ${LSB_RELEASE} |tee 1>/dev/null -a ${OSSARC}
 echo export OSSA_HOST=$(grep -oP '(?<=madison\.)[^$]+' <<< "${OSSA_ARCHIVE//.tgz}")|tee 1>/dev/null -a ${OSSARC}
 [[ -s ${OSSA_RC} ]] && source ${OSSA_RC}
+
 # Create manifest
 [[ -s ${DPKG_L} ]] && { awk '/^ii/{print $2"\t"$3}' ${DPKG_L}|tee 1>/dev/null ${OSSA_WORKDIR}/manifest.${OSSA_HOST}; }
 echo "export OSSA_MANIFEST=${OSSA_WORKDIR}/manifest.${OSSA_HOST}"|tee 1>/dev/null -a ${OSSARC}
+
+# source updated rc file
 [[ -s ${OSSA_RC} ]] && source ${OSSA_RC}
-show-release-info() {
-(printf "\nRelease|Code Name|Release Type|Standard Support Ends In...|ESM Ends In...\n"
-(UBU_URLS=( http://releases.ubuntu.com http://old-releases.ubuntu.com/releases )
-printf '%s\n' ${UBU_URLS[@]}|\
-(xargs -rn1 -P0 -I{} curl -sSlL {}|\
-awk -vRS=">|<" '/^Ubuntu.[0-9]+.*'${OSSA_CODENAME^}'/{gsub(/\(|\).*$|LTS |Beta /,"");gsub(/\.[0-9]$/,"",$2);split($2,a,/\./);print $0}'|sort -uV) |\
-while read -er DIST RELEASE CN1 CN2;do
-[[ $((${RELEASE%%\.*} % 2)) -eq 1 || ! ${RELEASE##*\.} = 04 ]] && { export FLAG=$(printf "\e[38;2;160;160;160;3mInterim\e[0m") SL=182 SD=days EL=0; }
-[[ $((${RELEASE%%\.*} % 2)) -eq 0 && ${RELEASE%%\.*} -le 10 && ${RELEASE%%\.*} -ge 6 ]] && [[ ${RELEASE##*\.} = 06 || ${RELEASE##*\.} = 04 ]] && { export FLAG=$(printf "LTS \e[38;2;160;160;160m(3 yrs)\e[0m") SL=3 SD=years EL=0; }
-[[ $((${RELEASE%%\.*} % 2)) -eq 0 && ${RELEASE%%\.*} -le 16 && ${RELEASE%%\.*} -ge 12 && ${RELEASE##*\.} = 04 ]] && { export FLAG=$(printf "LTS \e[38;2;160;160;160m(5 yrs)\e[0m") SL=5 SD=years EL=8; }
-[[ $((${RELEASE%%\.*} % 2)) -eq 0 && ${RELEASE%%\.*} -ge 18 && ${RELEASE##*\.} = 04 ]] && { export FLAG=$(printf "LTS \e[38;2;160;160;160m(10 yrs)\e[0m") SL=5 SD=years EL=10; }
-EOS_END=$(date -d "$(date -d "$([[ ${RELEASE%%\.*} -le 99 ]] && echo -n $((${RELEASE%%\.*}+2000)) || echo -n $((${RELEASE%%\.*}+3000)))$(printf '%02d' $((10#${RELEASE##*\.}+1)))01 -1 day" +%Y%m%d) +${SL} ${SD}" +%Y%m%d)
-EOL_END=$(date -d "$(date -d "$([[ ${RELEASE%%\.*} -le 99 ]] && echo -n $((${RELEASE%%\.*}+2000)) || echo -n $((${RELEASE%%\.*}+3000)))$(printf '%02d' $((10#${RELEASE##*\.}+1)))01 -1 day" +%Y%m%d) +${EL} ${SD}" +%Y%m%d)
-EOS_DAYS_REMAINING=$((($(date -d ${EOS_END} +%s)-$(date +%s))/(60*60*24)))
-[[ ${EL} = 0 ]] && EOL_DAYS_REMAINING=0 || EOL_DAYS_REMAINING=$((($(date -d ${EOL_END} +%s)-$(date +%s))/(60*60*24)))
 
-if [[ ${EOS_DAYS_REMAINING} -le 0 ]];then
-EOS_DAYS_REMAINING=$(printf "\e[38;2;160;160;160;3mExpired\e[0m")
-elif [[ ${EOS_DAYS_REMAINING} -le 30 && ${EOS_DAYS_REMAINING} -gt 0 ]];then
-EOS_DAYS_REMAINING=$(printf "\e[38;2;255;0;0m${EOS_DAYS_REMAINING} days ($(date -d "$(date +${EOS_END})" '+%d/%B/%Y'))\e[0m")
-elif [[ ${EOS_DAYS_REMAINING} -lt 365 && ${EOS_DAYS_REMAINING} -gt 0 ]];then
-EOS_DAYS_REMAINING=$(printf "\e[38;2;255;225;0m${EOS_DAYS_REMAINING} days ($(date -d "$(date +${EOS_END})" '+%d/%b/%Y'))\e[0m")
-elif [[ ${EOS_DAYS_REMAINING} -gt 365 && ${EOS_DAYS_REMAINING} -gt 0 ]];then
-EOS_DAYS_REMAINING=$(printf "\e[38;2;0;235;0m${EOS_DAYS_REMAINING} days ($(date -d "$(date +${EOS_END})" '+%d/%b/%Y'))\e[0m")
-fi
 
-if [[ ${EOL_DAYS_REMAINING} -le 0 ]];then
-EOL_DAYS_REMAINING=$(printf "\e[38;2;160;160;160;3mExpired\e[0m")
-elif [[ ${EOL_DAYS_REMAINING} -le 30 ]];then
-EOL_DAYS_REMAINING=$(printf "\e[38;2;225;0;0m${EOL_DAYS_REMAINING} days ($(date -d "$(date +${EOL_END})" '+%d/%b/%Y'))\e[0m")
-elif [[ ${EOL_DAYS_REMAINING} -lt 365 ]];then
-EOL_DAYS_REMAINING=$(printf "\e[38;2;255;200;0m${EOL_DAYS_REMAINING} days ($(date -d "$(date +${EOL_END})" '+%d/%b/%Y'))\e[0m")
-elif [[ ${EOL_DAYS_REMAINING} -gt 365 ]];then
-EOL_DAYS_REMAINING=$(printf "\e[38;2;0;235;0m${EOL_DAYS_REMAINING} days ($(date -d "$(date +${EOL_END})" '+%d/%b/%Y'))\e[0m")
-fi
-[[ ${EL} = 0 ]] && EOL_DAYS_REMAINING=$(printf "\e[38;2;160;160;160;3mN/A\e[0m")
-echo "$DIST $RELEASE|$CN1 $CN2|$FLAG|${EOS_DAYS_REMAINING}|${EOL_DAYS_REMAINING}"
-done))|column -nexts"|"|sed '2s/^.*$/'$(printf "\e[1m&\e[0m")'/g'
-echo
-};export show-release-info
+[[ -f ${OSSA_WORKDIR}/release-table.ansi && ${OSSA_WORKDIR}/release-table.txt ]] || { show-release-info|tee ${OSSA_WORKDIR}/release-table.ansi|sed 2>/dev/null 's/\x1b\[[0-9;]*[a-zA-Z]//g'|tee 1>/dev/null ${OSSA_WORKDIR}/release.table.txt; }
+([[ -f ${OSSA_WORKDIR}/release-table.ansi ]] && echo "export OSSA_RELEASE_TABLE_ANSI=${OSSA_WORKDIR}/release-table.ansi"|tee 1>/dev/null -a ${OSSA_RC})
+([[ -f ${OSSA_WORKDIR}/release-table.txt ]] && echo "export OSSA_RELEASE_TABLE=${OSSA_WORKDIR}/release-table.txt"|tee 1>/dev/null -a ${OSSA_RC})
 
-show-release-info
 # Show package breakdown by component and suite
 if [[ -f ${APT_MADISON} ]];then
 	declare -ag COMPONENTS=(main universe multiverse restricted)
@@ -136,6 +100,7 @@ if [[ -f ${APT_MADISON} ]];then
 		-re 's/multiverse.*$|restricted.*$/'$(printf "\e[38;2;255;0;0m")'&'$(printf "\e[0m")'/g')|sed 's/^.*$/ &/g'|tee ${OSSA_WORKDIR}/package_table.ansi
 		printf '\n\n'
 fi
+
 #cp ${OSSA_MANIFEST} ${PROG_DIR}
 #printf "Running cvescan -f ${PROG_DIR}/${OSSA_MANIFEST##*/} -p all\n"
 #cvescan -f ${PROG_DIR}/${OSSA_MANIFEST##*/}
@@ -146,7 +111,7 @@ fi
 printf "\e[2GChecking the availbility of OVAL Data for Ubuntu ${OSSA_CODENAME}\n"
 printf "\e[4GOVAL URL: https://people.canonical.com/~ubuntu-security/oval/oci.com.ubuntu.${OSSA_CODENAME}.cve.oval.xml.bz2\n"
 export OVAL_URI="https://people.canonical.com/~ubuntu-security/oval/oci.com.ubuntu.${OSSA_CODENAME}.cve.oval.xml.bz2"
-export TEST_OVAL=$(curl -slSL --connect-timeout 5 --max-time 20 --retry 5 --retry-delay 1 -w %{http_code} -o /dev/null ${OVAL_URI} 2>&1)
+export TEST_OVAL=$(curl -slSL --connect-timeout ${C} --max-time ${M} --retry 2 --retry-delay 2 -w %{http_code} -o /dev/null ${OVAL_URI} 2>&1)
 [[ ${OSSA_DEBUG} = true ]] && { echo "${TEST_OVAL}"; }
 if [[ ${TEST_OVAL:(-3)} -eq 404 ]];then
 	printf "\e[2GOVAL data file for Ubuntu ${OSSA_CODENAME^} is not available. Skipping download.\n"
@@ -158,11 +123,11 @@ fi
 # PERFORM CVE SCAN #
 ####################
 
-
 if [[ ${TEST_OVAL:(-3)} -eq 200 ]];then
 	printf "\e[4GInitiating CVE Scan:\n\e[6GOVAL Data OS: Ubuntu ${OSSA_CODENAME^}\n"
-	curl -slSL --connect-timeout 10 --max-time 30 --retry 3 --retry-delay 2 ${OVAL_URI} -o- |bunzip2 -d|tee 1>/dev/null ${OSSA_WORKDIR}/$(basename ${OVAL_URI//.bz2})
+	curl -slSL --connect-timeout ${C} --max-time ${M} --retry 2 --retry-delay 2 ${OVAL_URI} -o- |bunzip2 -d|tee 1>/dev/null ${OSSA_WORKDIR}/$(basename ${OVAL_URI//.bz2})
 printf "\e[6GOVAL Scan Type:  Package manifest\n\e[6GOrigin Hostname: ${OSSA_HOST}\n\e[6GOrigin Codename: ${OSSA_CODENAME^}\n\n"
 	oscap oval eval --report ${OSSA_WORKDIR}/oscap-cve-scan-report.${OSSA_HOST,,}.html ${OSSA_WORKDIR}/$(basename ${OVAL_URI//.bz2})|awk -vF=0 -vT=0 '{if ($NF=="false") F++} {if ($NF=="true") T++} END {print "CVE Scan Results (Summary)\n  Common Vulnerabilities Addressed: "F"\n  Current Vulnerability Exposure: "T}'|tee ${OSSA_WORKDIR}/cve-stats.${OSSA_HOST,,}|sed 's/^.*$/ &/g'
 fi
+
 echo;echo
